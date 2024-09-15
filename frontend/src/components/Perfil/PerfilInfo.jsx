be310c757e5c8352c09eee207b0619f1a9399bc9
alezcf <ialeczander@gmail.com>
2024-09-15 15:39:15 -0300

    fix Perfil screen and add css

--- frontend/src/components/Perfil/PerfilInfo.jsx
+++ frontend/src/components/Perfil/PerfilInfo.jsx
@@ -1,34 +1,43 @@
-// src/pages/Perfil/Perfil.js
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import PerfilInfo from '../../components/Perfil/PerfilInfo';
-import { getUsuario } from '../../services/usuario.service';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, ListGroup } from 'react-bootstrap';
+import '../../css/PerfilInfo.css';
 
-const Perfil = () => {
-    const { usuarioId } = useParams();
-    const [perfilData, setPerfilData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        const fetchUsuario = async () => {
-        try {
-            const data = await getUsuario(usuarioId);
-            setPerfilData(data);
-            setLoading(false);
-        } catch (err) {
-            setError('Error al cargar el perfil');
-            setLoading(false);
-        }
-        };
-
-        fetchUsuario();
-    }, [usuarioId]);
-
-    if (loading) return <p>Cargando...</p>;
-    if (error) return <p>{error}</p>;
-
-    return perfilData ? <PerfilInfo perfilData={perfilData} /> : null;
+const PerfilInfo = ({ perfilData }) => {
+    return (
+        <Card className="perfil-card">
+            <Card.Header className="perfil-card-header">
+                <h2>Perfil del Usuario</h2>
+            </Card.Header>
+            <ListGroup variant="flush">
+                <ListGroup.Item className="perfil-list-group-item">
+                    <strong>Nombre Completo: </strong>{perfilData.nombreCompleto}
+                </ListGroup.Item>
+                <ListGroup.Item className="perfil-list-group-item">
+                    <strong>RUT: </strong>{perfilData.rut}
+                </ListGroup.Item>
+                <ListGroup.Item className="perfil-list-group-item">
+                    <strong>Email: </strong>{perfilData.email}
+                </ListGroup.Item>
+                <ListGroup.Item className="perfil-list-group-item">
+                    <strong>Rol: </strong>{perfilData.rol}
+                </ListGroup.Item>
+                <ListGroup.Item className="perfil-list-group-item">
+                    <strong>Registro de Cuenta: </strong>{perfilData.createdAt}
+                </ListGroup.Item>
+            </ListGroup>
+        </Card>
+    );
 };
 
-export default Perfil;
+PerfilInfo.propTypes = {
+    perfilData: PropTypes.shape({
+        nombreCompleto: PropTypes.string.isRequired,
+        rut: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+        rol: PropTypes.string.isRequired,
+        createdAt: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default PerfilInfo;
--- frontend/src/routes/Perfil/Perfil.jsx
+++ frontend/src/routes/Perfil/Perfil.jsx
@@ -7,26 +7,26 @@ import { getUsuario } from '../../services/usuario.service';
 const Perfil = () => {
     const { usuarioId } = useParams();
     const [perfilData, setPerfilData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true); // Estado para manejar la carga
+    const [error, setError] = useState(null); // Estado para manejar errores
 
     useEffect(() => {
         const fetchUsuario = async () => {
-        try {
-            const data = await getUsuario(usuarioId);
-            setPerfilData(data);
-            setLoading(false);
-        } catch (err) {
-            setError('Error al cargar el perfil');
-            setLoading(false);
-        }
+            try {
+                const data = await getUsuario(usuarioId);
+                setPerfilData(data);
+                setLoading(false); // Cambiar a 'false' una vez que los datos se cargan
+            } catch (err) {
+                setError('Error al cargar el perfil');
+                setLoading(false);
+            }
         };
 
         fetchUsuario();
-    }, [usuarioId]);
+    }, [usuarioId]); // Dependencia de 'usuarioId', la función solo se ejecuta cuando cambia
 
-    if (loading) return <p>Cargando...</p>;
-    if (error) return <p>{error}</p>;
+    if (loading) return <p>Cargando...</p>; // Mostrar un mensaje de carga mientras se obtienen los datos
+    if (error) return <p>{error}</p>; // Mostrar el mensaje de error en caso de fallo
 
     return perfilData ? <PerfilInfo perfilData={perfilData} /> : null;
 };
